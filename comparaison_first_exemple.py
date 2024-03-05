2334c3de62dd9c3028d0556ef315b30f28e850ea
omnia02l <omnia.lahdhiri@esprit.tn>
2024-03-06 00:32:26 +0100

    pss

--- comparaison_first_exemple.py
+++ comparaison_first_exemple.py
@@ -49,3 +49,4 @@
  h))
    k3_x_rk4 = c * tau * (urk4[i] + k2_x_rk4 / 2)
    k4_t_rk4 = tau * (source(x_valeurs[i] , 0.75 + tau) - c * (urk4[i] + k3_x_rk4 - urk4[i-1] - k3_t_rk4) / (2 * h))
+   k4_x_rk4 = c * tau * (urk4[i] + k3_x_rk4)
