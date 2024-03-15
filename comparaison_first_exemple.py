4eab12c1180b4ee443fb14f6dfa99a9c3b1b6868
omnia02l <omnia.lahdhiri@esprit.tn>
2024-03-15 23:34:43 +0100

    up

--- comparaison_first_exemple.py
+++ comparaison_first_exemple.py
@@ -55,4 +55,4 @@
  diff_rk4 = np.abs(urk4 - udiff)
  //Visualisation des graphes
 plt.figure(figsize=(10, 6))
-plt.plot(x_valeurs, diff_rk2plt.plot(x_valeurs, diff_rk2, label='Écart RK2')
+plt.plot(x_valeurs, diff_rk2plt.plot(x_valeurs, diff_rk2, label='Écart RK2', linestyle='dashed')
