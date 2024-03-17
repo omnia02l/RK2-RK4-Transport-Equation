03ece5bb269a1c1be3414fe6105697c27af7ddb7
omnia02l <omnia.lahdhiri@esprit.tn>
2024-03-17 20:26:20 +0100

    màj

--- comparaison_first_exemple.py
+++ comparaison_first_exemple.py
@@ -56,4 +56,4 @@
  //Visualisation des graphes
 plt.figure(figsize=(10, 6))
 plt.plot(x_valeurs, diff_rk2, label='Écart RK2', linestyle='dashed')
-plt.plot(x_valeurs, diff_rk4, label='Écart RK4')
+plt.plot(x_valeurs, diff_rk4, label='Écart RK4', linestyle='dashed')
