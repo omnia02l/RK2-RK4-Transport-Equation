82b36c17a67ace559640b15d14162122bd504b08
omnia02l <omnia.lahdhiri@esprit.tn>
2024-04-05 20:38:46 +0100

    re

--- solution_second_exemple.py
+++ solution_second_exemple.py
@@ -16,5 +16,5 @@ def source(x, t):
  return np.exp(t**2)+x
 udiff = np.zeros(len(x_valeurs))
 urk2 = np.zeros(len(x_valeurs))
-urk4 = np.zeros(len(x))
+urk4 = np.zeros(len(x_val))
 for i in range():
